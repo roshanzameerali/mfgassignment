772bc83906674403821a369615344fc7a2fc2612
unknown <roshan.zameeralis@lntinfotech.com>
2021-03-03 17:58:13 +0530

    already present password validation 20 percent

--- EmployeeManagementSystemSolution/EmployeeManagementSystem/Controllers/RegistrationController.cs
+++ EmployeeManagementSystemSolution/EmployeeManagementSystem/Controllers/RegistrationController.cs
@@ -12,12 +12,27 @@ namespace EmployeeManagementSystem.Controllers
     [EnableCors(origins: "http://localhost:4200", headers: "*", methods: "*")]
     public class RegistrationController : ApiController
     {
-        [HttpPost]
-        public void RegisterEmployee(tblEmployee Employee)
+        dbEmployeesSystemEntities1 entities = new dbEmployeesSystemEntities1();
+        public IEnumerable<tblEmployee> Get()
         {
-            dbEmployeesSystemEntities1 entities = new dbEmployeesSystemEntities1();
-            entities.tblEmployees.Add(Employee);
-            entities.SaveChanges();
+            return entities.tblEmployees.ToList();
+        }
+        [HttpPost]
+        public HttpResponseMessage RegisterEmployee(tblEmployee Employee)
+        {
+            string result = null;
+            result = entities.proc_CheckAlreadyExistingEmployee(Employee.mobile_number).FirstOrDefault();
+            if(result == null)
+            {
+                entities.tblEmployees.Add(Employee);
+                entities.SaveChanges();
+            }
+            else
+            {
+                return Request.CreateResponse("Employee already exists");
+            }
+            var Employee_Id = entities.proc_GenerateEmployee_id(Employee.Employee_id);
+            return Request.CreateResponse(Employee_Id);
         }
 
     }
